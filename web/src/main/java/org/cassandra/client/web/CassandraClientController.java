3b4d5887b168880d62357e6f3bd9fda70d028e97
no spam <no.spam.period@gmail.com>
2021-11-27 23:39:02 -0800

    react

--- web/src/main/java/org/cassandra/client/web/CassandraClientController.java
+++ web/src/main/java/org/cassandra/client/web/CassandraClientController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"}, maxAge = 3600)
 @RequestMapping(path = "/api/cassandra", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CassandraClientController {
     private final CassandraClient client;
